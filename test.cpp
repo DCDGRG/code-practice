7b5cd3fa5c5a4f4e7e89d1426115d802eff86bdd
Dawen.W <dawener@air.local>
2024-07-25 13:58:58 +0800

    C++ configuration test

---
+++ test.cpp
@@ -0,0 +1,11 @@
+// A simple test to validate the functionality of the library, print hello world
+
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string hello = "Hello, World!";
+    std::cout << hello << std::endl;
+    return 0;
+}
+
